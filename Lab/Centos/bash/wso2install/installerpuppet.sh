61ecdf7821bc9937dadbc9d11050c3f935b523cf
Jean-Claude Parent <noursicain@gmail.com>
2019-10-11 19:21:35 -0500

    Rapatrier les scripts bash a root

--- Lab/Centos/bash/wso2install/installerpuppet.sh
+++ Lab/Centos/bash/wso2install/installerpuppet.sh
@@ -1,3 +1,3 @@
 puppet module install puppetlabs-git --version 0.5.0
-puppet apply module git
+
 
--- Lab/Centos/puppet/manifests/zose.pp
+++ Lab/Centos/puppet/manifests/zose.pp
@@ -1,11 +1,10 @@
+mod 'puppetlabs-git', '0.5.0'
+
+
 
 class base
 {
   
 
    notify { 'Installation des outils de base pour travailler':}
-  include git
-
 }
-
-include '::base'
