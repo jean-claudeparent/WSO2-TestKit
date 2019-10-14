1bd056831598c2fd121f070654b67acc52cb0f80
Jean-Claude Parent <noursicain@gmail.com>
2019-10-14 08:12:59 -0500

    Reculer l'instakllation des modules jenkins 1.0

--- Lab/Centos/bash/root/loadmod.sh
+++ Lab/Centos/bash/root/loadmod.sh
@@ -1,11 +1,5 @@
 puppet module install puppetlabs-git --version 0.5.0
 puppet module install puppetlabs-java --version 5.0.1
-echo install les dependances de jenkins
-puppet module install puppetlabs-stdlib --version 6.1.0
-puppet module install puppetlabs-apt --version 7.1.0
-puppet module install darin-zypprepo --version 1.0.2
-puppet module install puppet-archive --version 4.2.0
-puppet module upgrade
-echo Jenkins
-puppet module install rtyler-jenkins --version 1.7.0
+
+
 
